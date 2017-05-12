c0b9f58b8870e767cc913b526e9634f29e473030
jay945 <YM@YMdeMacBook-Pro.local>
2017-05-12 10:39:16 +0800

    添加Kit
    添加Kit测试

---
+++ TXMyKit/MyClass.h
@@ -0,0 +1,15 @@
+//
+//  MyClass.h
+//  TXMyPrj
+//
+//  Created by YM on 2017/5/12.
+//  Copyright © 2017年 Tincent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyClass : NSObject
+
+-(void)printTest;
+
+@end
---
+++ TXMyKit/MyClass.m
@@ -0,0 +1,18 @@
+//
+//  MyClass.m
+//  TXMyPrj
+//
+//  Created by YM on 2017/5/12.
+//  Copyright © 2017年 Tincent. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+-(void)printTest {
+    
+    NSLog(@"This is my kit");
+}
+
+@end
